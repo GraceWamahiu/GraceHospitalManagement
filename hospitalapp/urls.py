1a82279f818eb108a3fd382e10e9fe12c33da026
Grace <157009110+GraceWamahiu@users.noreply.github.com>
2024-02-27 13:38:57 +0300

    Add files via upload

--- hospitalapp/urls.py
+++ hospitalapp/urls.py
@@ -6,6 +6,10 @@ from hospitalapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('innerpage/', views.index, name='inner')
+    path('innerpage/', views.index, name='inner'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('upload/', views.upload, name='upload')
+
 
 ]
