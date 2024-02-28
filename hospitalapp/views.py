730f94a16d124134dc89f0d620c7a45357d87472
Grace <157009110+GraceWamahiu@users.noreply.github.com>
2024-02-28 13:40:27 +0300

    Add files via upload

--- hospitalapp/views.py
+++ hospitalapp/views.py
@@ -1,7 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from hospitalapp.models import Member, Message, Users
+
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        messages = Message(name=request.POST['name'],
+                           email=request.POST['email'],
+                           subject=request.POST['subject'],
+                           message=request.POST['message'])
+        messages.save()
+        return redirect('/')
+    else:
+        return render(request, 'index.html')
+
+
 def inner(request):
     return render(request, 'inner-page.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        member = Member(username=request.POST['username'], email=request.POST['email'],
+                        password=request.POST['password'])
+        member.save()
+        return redirect('/register')
+    else:
+        return render(request, 'register.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def upload(request):
+    return render(request, 'upload.html')
+
+
+def detail(request):
+    details = Message.objects.all()
+    return render(request, 'details.html', {'details': details})
+
+
+def user(request):
+    user = Users.objects.all()
+    return render(request, 'users.html', {'users': user})
